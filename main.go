6684dbcbd30f681be247e087b670f48fce67dd12
Tirdad Abbasi <abbasi.tirdad@gmail.com>
2025-03-28 14:13:36 +0100

    mongoDB added

--- main.go
+++ main.go
@@ -1,20 +1,40 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+var DB *mongo.Database
+
+func intiMongo() {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(os.Getenv("MONGO_URI")))
+	if err != nil {
+		log.Fatal("MongoDB connection error: ", err)
+	}
+
+	DB = client.Database(os.Getenv("MONGO_DB"))
+	fmt.Println("✅ MongoDB connected.")
+}
+
 func main() {
+
+	intiMongo()
 
 	router := gin.Default()
 
 	router.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "Hello World",
-		})
+		c.JSON(http.StatusOK, gin.H{"message": "User API is running 🚀"})
 	})
 
 	port := os.Getenv("PORT")
